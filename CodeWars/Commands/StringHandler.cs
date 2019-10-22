48aedba2d59b33b03916191bd703dbca33c2b4a7
Ross, Ryan <ryanross@amrock.com>
2019-10-22 10:29:22 -0400

    Adding the things

---
+++ CodeWars/Commands/StringHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.Commands
+{
+    public class StringHandler
+    {
+        public int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            List<string> vowels = new List<string>()
+            {
+                "A", "E", "I", "O", "U"
+            };
+
+            foreach(char letter in str)
+            {
+                vowelCount += vowels.Count(ltr => ltr == letter.ToString().ToUpper());
+            }
+
+            return vowelCount;
+        }
+    }
+}
